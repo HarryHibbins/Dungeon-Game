d203662145cd3fb8fc7a0c7206fd0ad570389970
Tom <fudgeman3000@gmail.com>
2021-11-22 22:43:32 +0000

    Added crude torch timer mechanic
    -Torch now slowly burns out when player picks it up
    -only works for one of the torches atm due to naming issues
    -progress bar in corner to show torch time left

--- Dungeon Game Unity/Assets/Scripts/FixedTorch.cs
+++ Dungeon Game Unity/Assets/Scripts/FixedTorch.cs
@@ -9,20 +9,28 @@ public class FixedTorch : MonoBehaviour
     private PlayerController player;
     private GameObject playerTorchHolderObj;
     private Transform playerTorchHolder;
-    private bool canPickUpTorch;
+    public bool canPickUpTorch;
+    public bool hasTorch;
+    public float torchTimer = 30;
+    public GameObject flame;
     void Start()
     {
         playerObj = GameObject.FindWithTag("Player");
         player = playerObj.GetComponent<PlayerController>();
         playerTorchHolderObj = GameObject.FindWithTag("Flame Holder");
         playerTorchHolder = playerTorchHolderObj.transform;
+        flame = this.gameObject.transform.Find("Torch (1)").gameObject;
     }
 
     void Update()
     {
-        if (canPickUpTorch && Input.GetButtonDown("Interact"))
+        if (canPickUpTorch && Input.GetButtonDown("Interact") && !hasTorch)
         {
+            flame = this.gameObject.transform.Find("Torch (1)").gameObject;
             Debug.Log("Pick up torch");
+
+            hasTorch = true;
+            torchTimer = 30;
           
             transform.position = playerTorchHolder.transform.position;
             
@@ -30,6 +38,12 @@ public class FixedTorch : MonoBehaviour
           //  transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
         }
 
+        if (hasTorch) 
+        {
+            torchTimer -= Time.deltaTime;
+            flame.GetComponent<Light>().range = (torchTimer / 30) * 10;
+                    
+        }
 
 
     }
