f8eac09a6d049777bae9877e66c919cad2e8187e
Cory Goodhind <cory2.goodhind@live.uwe.ac.uk>
2021-12-01 11:12:08 +0000

    Added enemy prefab

---
+++ Dungeon Game Unity/Assets/RoomGeneration 1/RoomSpawner.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 -> need bottom door
+    // 2 -> need top door
+    // 3 -> need left door
+    // 4 -> need right door
+
+    private RoomTemplates templates;
+    private int rand;
+    public bool spawned = false;
+
+    public float waitTime = 4f;
+
+    private void Start()
+    {
+        Destroy(gameObject, waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.5f);
+    }
+
+    private void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                // Need bottom door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                // Need top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                // Need left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                // Need right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+            templates.waitTime = templates.startWaitTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            if (!other.GetComponent<RoomSpawner>().spawned && !spawned)
+            {
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+
+}
