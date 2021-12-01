9af87c791602ae80ac1f173f1ebb514ffd554626
Harry <harrison2.hibbins@live.uwe.ac.uk>
2021-12-01 15:53:28 +0000

    Implemented spawning system and fixed enemies
    -Need to add spawn points to each room
    
    -Higher chance of spawning a normal enemy over tank or fast, if we are having other types i will have to rework the spawn chances because its hardcoded atm
    
    -Sometimes enemies spawn on the ceiling, i think its because the navmesh is walkable up there and not on certain rooms so its the closest place.

---
+++ Dungeon Game Unity/Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Networking.PlayerConnection;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    private int numOfEnemies;
+
+    private GameObject gameManager;
+    private EnemyTypes enemyTypes;
+
+    [SerializeField] private int minEnemiesInRoom = 2;
+    
+    //Chance of spawning enemies per spawn point between 0 and 1
+    [SerializeField] private float spawnChance = 0.5f;
+    
+   
+    
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameManager");
+        enemyTypes = gameManager.GetComponent<EnemyTypes>();
+        
+
+        foreach (Transform child in transform)
+        {
+            
+            if (child.tag == "Enemy Spawn Point")
+            {
+                float randomValue = Random.value;
+              
+                if ( randomValue <= spawnChance ) {
+                   Instantiate(getEnemyType(), child.position, child.rotation);
+                   numOfEnemies++;
+
+                }
+                else {
+                    //minimum of 2 enemies per room
+                    if (numOfEnemies < minEnemiesInRoom)
+                    {
+                        Instantiate(getEnemyType(), child.position, child.rotation);
+
+                        numOfEnemies++;
+                    }
+                }
+            }
+
+        }  
+    }
+
+    GameObject getEnemyType()
+    {
+        GameObject enemyToSpawn;
+
+
+        float randomValue = Random.Range(1, 10);
+              
+        if ( randomValue <= 2 ) 
+        {
+            //Spawn fast enemy
+            enemyToSpawn = enemyTypes.enemies[1];
+            Debug.Log("Spawn Fast enemy");
+        }
+        else if (randomValue > 2 && randomValue <= 4)
+        {
+            //Spawn tank enemy
+            enemyToSpawn = enemyTypes.enemies[2];
+            Debug.Log("Spawn Tank enemy");
+
+        }
+        /*if (Random.Range(0, enemyTypes.enemies.Length) == 1)
+        {
+            enemyToSpawn = enemyTypes.enemies[1];
+        }*/
+        else
+        {
+            //Spawn normal enemy
+            enemyToSpawn = enemyTypes.enemies[0];
+            Debug.Log("Spawn Normal enemy");
+
+            
+        }
+        
+        
+
+        return enemyToSpawn;
+
+
+
+    }
+
+}
---
+++ Dungeon Game Unity/Assets/Scripts/EnemyTypes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]  
+public class EnemyTypes : MonoBehaviour
+{
+    /*public enum Enemies
+    {
+        Normal,
+        Fast,
+        Tank,
+        Boss,
+        
+    }*/
+
+  
+    public GameObject[] enemies;
+    
+    public GameObject BossEnemy;
+}
