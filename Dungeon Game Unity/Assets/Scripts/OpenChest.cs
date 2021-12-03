2d2475d2fcd9236ec9922ac76635aa4532d296ad
Dan Millward <daniel2.millward@live.uwe.ac.uk>
2021-12-03 17:37:20 +0000

    Added Chest animation

--- Dungeon Game Unity/Assets/Scripts/OpenChest.cs
+++ Dungeon Game Unity/Assets/Scripts/OpenChest.cs
@@ -6,12 +6,16 @@ public class OpenChest : MonoBehaviour
 {
     private bool inRange = false;
     private bool opened = false;
+    public AnimationClip animationClip;
+    private Animation animation;
     private GameLoot gameLoot;
     public GameObject lootpos;
 
     private void Awake()
     {
         gameLoot = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameLoot>();
+        
+        animation = GetComponent<Animation>();
     }
 
     private void Update()
@@ -44,6 +48,8 @@ public class OpenChest : MonoBehaviour
     void OpenLootChest()
     {
         //Play chest animation
+        animation.clip = animationClip;
+        animation.Play();
         Vector3 chest_location = this.transform.position;
         gameLoot.SpawnLoot(lootpos.transform.position, gameLoot.getLootByRarityToSpawn(gameLoot.RandomRarity()));
         opened = true;
